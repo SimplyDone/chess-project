351fa397761adbd148f33f3a85d4d0156170c620
Alex <Alex@192.168.1.66>
2017-12-02 01:41:21 -0500

    Testing out commit to see how it works.

---
+++ Chess Project/src/chess/project/ChessProject.java
@@ -0,0 +1,19 @@
+
+package chess.project;
+
+/**
+ *
+ * @author Alex
+ */
+public class ChessProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Testing");
+        
+    }
+
+}
