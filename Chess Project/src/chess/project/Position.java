f8545265a5b7fdf92c642fd4e9e8d0e5b89ae68f
Alex Zurad <az15hh@brocku.ca>
2017-12-07 20:42:52 -0500

    Updated various piece class structure.

---
+++ Chess Project/src/chess/project/Position.java
@@ -0,0 +1,22 @@
+package chess.project;
+
+/**
+ *
+ * @author Alex Zurad
+ */
+public class Position {
+    
+    public int X;
+    public int Y;
+    
+    public Position(int x, int y){
+        X = x;
+        Y = y;
+    }
+    
+    public void setPosition(int x, int y){
+        X = x;
+        Y = y;
+    }
+    
+}
