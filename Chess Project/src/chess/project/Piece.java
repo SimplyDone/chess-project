648d8c73a38f0dd338db7264a096980c14f8015b
Alex Zurad <az15hh@brocku.ca>
2017-12-07 20:03:12 -0500

    Added Piece class

--- Chess Project/src/chess/project/Piece.java
+++ Chess Project/src/chess/project/Piece.java
@@ -1,18 +1,27 @@
 
 package chess.project;
 
-/** 
+import java.util.List;
+
+/** Represents a single piece.
  *
  * @author Alex Zurad
  */
 public abstract class Piece {
     
-    private boolean colour;
+    private final boolean colour;
     private int xPos;
+    private int yPos;
     
     
-    public Piece(){
-        
+    public Piece(boolean col){
+        colour = col;
+         
     }
+    
+    protected abstract void setInitalPosition();
+    
+    protected abstract List getValidMoves(Board board);
+       
     
 }
