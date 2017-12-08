d6d3b60d88a9438e8724d005d864ce931c12ee23
Alex Zurad <az15hh@brocku.ca>
2017-12-08 02:49:44 -0500

    Added getColour method that returns the colour of the piece.

--- Chess Project/src/chess/project/Piece.java
+++ Chess Project/src/chess/project/Piece.java
@@ -10,14 +10,21 @@ import java.util.List;
 public abstract class Piece {
     
 
-    private final boolean colour; // 0-black, 1-white
+    private final boolean colour; // false-black, true-white
     private Position pos;
        
     public Piece(boolean col, Position pos){
-        colour = col;
-         
+        colour = col;  
     }
     
     protected abstract List getValidMoves(Board board);
+    
+    /** Returns the colour of the piece;
+     * 
+     * @return the colour of the piece
+     */
+    public String getColour(){
+        return colour ? "white" : "black";
+    }
     
 }
