c365ff9aaf43ba9a96e2b5386ce5e5f695da883b
Alex Zurad <az15hh@brocku.ca>
2018-01-07 22:54:13 -0500

    Did graphic stuff.

--- Chess Project/src/chess/project/players/AIPlayer.java
+++ Chess Project/src/chess/project/players/AIPlayer.java
@@ -11,7 +11,6 @@ import java.util.*;
  */
 public class AIPlayer extends Player {
 
-    private Board testBoard;
     private final int difficulty;
 
     public AIPlayer(ChessColour colour, Board board, int difficulty) {
@@ -23,46 +22,19 @@ public class AIPlayer extends Player {
     public Move getMove() {
 
         System.out.println(colour);
-        int[] v = miniMax(difficulty, board, true);
+        int[] v = miniMax(difficulty, board, true, Integer.MIN_VALUE, Integer.MAX_VALUE);
         Move m = new Move(new Position(v[1], v[2]), new Position(v[3], v[4]));
         System.out.println(m + " " + v[0]);
         return m;
 
-//        System.out.println(colour);
-//        System.out.println(miniMax(2, board, true));
-//        
-//        
-//        Move bestMove = null;
-//        double currentScore = -10000;
-//        double bestScore = -10000;
-//
-//        for (Move m : allValidMoves) {
-//
-//            if (bestMove == null) {
-//                bestMove = m;
-//            } else {
-//                testBoard.doMove(m);
-//                currentScore = evaluateBoard(testBoard.getBoard());
-//                if (bestScore < currentScore) {
-//                    bestMove = m;
-//                    bestScore = currentScore;
-//
-//                } else if (bestScore == currentScore && Math.random() < 0.5) {
-//                    bestMove = m;
-//                    bestScore = currentScore;
-//                }
-//            }
-//            testBoard = board.clone();
-//        }
-//
-//        System.out.println(bestMove + " " + bestScore);
-//        return bestMove;
     }
 
-    private int[] miniMax(int depth, Board b, boolean isMaxPlayer) {
+    private int[] miniMax(int depth, Board b, boolean isMaxPlayer, int alpha, int beta) {
 
         Board tempBoard;
         ChessColour c;
+        
+        //might need to be changed
         if (isMaxPlayer) {
             c = colour;
         } else {
@@ -79,34 +51,46 @@ public class AIPlayer extends Player {
         int currScore;
         Move bestMove = null;
 
+        //exit
         if (validMoves[0].isEmpty() || depth == 0) {
-            bestScore = evaluateBoard(b, validMoves);
-        } else {
-            if (isMaxPlayer) {
-                for (Move m : validMoves[0]) {
-                    tempBoard = b.clone();
+            return new int[]{bestScore, -1, -1, -1, -1};
+        }
+        
+        if (isMaxPlayer) {
+            for (Move m : validMoves[0]) {
+                tempBoard = b.clone();
 
-                    tempBoard.doMove(m);
-                    currScore = miniMax(depth - 1, tempBoard, !isMaxPlayer)[0];
+                tempBoard.doMove(m);
+                currScore = miniMax(depth - 1, tempBoard, !isMaxPlayer, alpha, beta)[0];
 
-                    if ((currScore > bestScore) || (currScore == bestScore && Math.random() < 0.05)) {
-                        bestScore = currScore;
-                        bestMove = m;
-                    }
-
+                if ((currScore > bestScore) || (currScore == bestScore && Math.random() < 0.05)) {
+                    bestScore = currScore;
+                    bestMove = m;
                 }
-            } else {
-                for (Move m : validMoves[0]) {
-                    tempBoard = b.clone();
+                
+                alpha = Math.max(alpha, bestScore);
+                
+                if(beta <= alpha){
+                    break;
+                }
 
-                    tempBoard.doMove(m);
-                    currScore = miniMax(depth - 1, tempBoard, !isMaxPlayer)[0];
+            }
+        } else {
+            for (Move m : validMoves[0]) {
+                tempBoard = b.clone();
 
-                    if ((currScore < bestScore) || (currScore == bestScore && Math.random() < 0.05)) {
-                        bestScore = currScore;
-                        bestMove = m;
-                    }
+                tempBoard.doMove(m);
+                currScore = miniMax(depth - 1, tempBoard, !isMaxPlayer, alpha, beta)[0];
 
+                if ((currScore < bestScore) || (currScore == bestScore && Math.random() < 0.05)) {
+                    bestScore = currScore;
+                    bestMove = m;
+                }
+                
+                beta = Math.min(beta, bestScore);
+                
+                if(beta <= alpha){
+                    break;
                 }
 
             }
@@ -119,7 +103,7 @@ public class AIPlayer extends Player {
 
             return new int[]{bestScore, oldPos.getX(), oldPos.getY(), newPos.getX(), newPos.getY()};
         } else {
-            return new int[]{bestScore, -1, -1, -1, -1};
+            return new int[]{bestScore, 0, 0, 0, 0};
         }
     }
 
@@ -139,18 +123,18 @@ public class AIPlayer extends Player {
                 }
             }
         }
-        return new List[] { validMoves, enemyMoves };
+        return new List[]{validMoves, enemyMoves};
     }
 
     public int evaluateBoard(Board b, List<Move>[] validMoves) {
-        int score = 0;
 
-        int modifier = 1;
+        int score = 0;
+        int modifier;
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
 
-                Piece p = board.getBoard()[i][j];
+                Piece p = b.getBoard()[i][j];
                 if (p != null) {
 
                     if (p.getColour() == colour) {
@@ -160,15 +144,15 @@ public class AIPlayer extends Player {
                     }
 
                     if (p instanceof King) {
-                        score += 2000 * modifier;
+                        score += 20000 * modifier;
                     } else if (p instanceof Queen) {
-                        score += 90 * modifier;
+                        score += 900 * modifier;
                     } else if (p instanceof Rook) {
-                        score += 50 * modifier;
+                        score += 500 * modifier;
                     } else if (p instanceof Bishop || p instanceof Knight) {
-                        score += 30 * modifier;
+                        score += 300 * modifier;
                     } else if (p instanceof Pawn) {
-                        score += 10 * modifier;
+                        score += 100 * modifier;
 
                         // score -= 0.5 * doubled, blocked, isolated
                     }
@@ -176,8 +160,18 @@ public class AIPlayer extends Player {
                 }
             }
         }
+
         score += 1 * validMoves[0].size();
         score -= 1 * validMoves[1].size();
+
+//        if (validMoves[0].isEmpty()) {
+//            score = Integer.MIN_VALUE;
+//        }
+//
+//        if (validMoves[1].isEmpty()) {
+//            score = Integer.MAX_VALUE;
+//        }
+
         return score;
     }
 
