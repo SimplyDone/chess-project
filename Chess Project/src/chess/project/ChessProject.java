64cf3f82d2e4be2eac1514011a83e95deb3f1cd6
Alex Zurad <az15hh@brocku.ca>
2018-01-17 02:13:23 -0500

    Lots of AI changes

--- Chess Project/src/chess/project/ChessProject.java
+++ Chess Project/src/chess/project/ChessProject.java
@@ -11,6 +11,26 @@ public class ChessProject {
      */
     public static void main(String[] args) {
 
+//        Board b = new Board();
+//
+//        long time = System.currentTimeMillis();
+//        for(int i = 0; i < 100000; i++){
+//            b.clone();
+//        }
+//        long value1 = System.currentTimeMillis() - time;
+//        System.out.println(value1);
+//        
+//        time = System.currentTimeMillis();
+//        for(int i = 0; i < 100000; i++){
+//            b.fastClone();
+//        }
+//        long value2 = System.currentTimeMillis() - time;
+//        System.out.println(value2);
+//        
+//        System.out.println(value1/value2 + " times faster");
+        
+        
+        
         GameHandler g = new GameHandler();
     }
 
